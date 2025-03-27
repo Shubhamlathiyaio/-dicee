2b42b612a938347edee2d7db5e1c6b224ccae159
Divol3tech <divol3.tech@gmail.com>
2025-03-27 13:27:00 +0530

    working dicee

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:dicee/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage(),);
+  }
+}
